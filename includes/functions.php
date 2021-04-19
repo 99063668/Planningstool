28f71aa3e45d1a7837558ff2ddffc9fd3402d06f
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-04-19 10:31:50 +0200

    Add files via upload

---
+++ includes/functions.php
@@ -0,0 +1,17 @@
+<?php
+    include("database.php");
+
+    function getAllGames(){
+        $conn = openDatabase();
+        $query = $conn->prepare("SELECT * FROM games");
+        $query->execute();
+        return $query->fetchAll();
+    }
+
+    function getGame($id){
+        $conn = openDatabase();
+        $query = $conn->prepare("SELECT * FROM games WHERE id = :id");
+        $query->bindParam(":id, $id");
+        $query->execute();
+        return $query->fetch();
+    }
