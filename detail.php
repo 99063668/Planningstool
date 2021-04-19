aa532a30d822160f107c87096b3e5e3307c956c2
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-04-19 10:30:13 +0200

    Add files via upload

---
+++ detail.php
@@ -0,0 +1,36 @@
+<?php
+    include("functions.php");
+
+    if(isset($_GET["id"])){
+        $game = getGame($_GET["id"]);
+        if(!$game){
+            header("location: index.php");
+        }
+    }else{
+        header("location: index.php");
+    }
+
+    include("common/header.php");
+?>
+
+<div class="row">
+    <div class="col-12"><h1><?=$game["name"]?></h1></div>
+    <div class="col-6">
+
+        <?=$game["description"]?>
+
+            <p><label class="w-25 font-weight-bold">Expansions</tabel><?=$game["expansions"]?></p>
+            <p><label class="w-25 font-weight-bold">Skills</tabel><?=$game["skills"]?></p>
+            <p><label class="w-25 font-weight-bold">Min spelers</tabel><?=$game["min-players"]?></p>
+            <p><label class="w-25 font-weight-bold">Max spelers</tabel><?=$game["max_players"]?></p>
+            <p><label class="w-25 font-weight-bold">Speeltijd</tabel><?=$game["play_minutes"]?></p>
+            <p><label class="w-25 font-weight-bold">Uitlegtijd</tabel><?=$game["explain_minutes"]?></p>
+            <p><label class="w-25 font-weight-bold">Website</tabel><a target="_blank" href="<?=$game["url"]?>">Klik hier</a></p>
+
+    </div>
+    
+        <div class="col-6"><img src="images/<?=$game["image"]?>" alt=""><br><br><?=$game["youtube"]?></div>
+
+</div>
+
+    
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    include("includes/functions.php");
+
+    $games = getAllGames();
+
+    include("common/header.php");
+?>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Naam</th>
+            <th scope="col">Afbeelding</th>
+            <th scope="col">Skills</th>
+            <th scope="col">Min spelers</th>
+            <th scope="col">Max spelers</th>
+            <th scope="col">Speeltijd</th>
+            <th scope="col">Uitlegtijd</th>
+            <th scope="col"> </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach($games as $game){
+        ?>
+        <tr>
+            <td><?=$game["name"]?></td>
+            <Td><img width="150" src="images/<?=$game["image"]?>" alt="<?=$game["name"]?>"><td>
+            <td><?=$game["skills"]?></td>
+            <td><?=$game["min_players"]?></td>
+            <td><?=$game["max_Players"]?></td>
+            <td><?=$game["play_minutes"]?></td>
+            <td><?=$game["explain_minutes"]?></td>
+            <td><a href="detail.php?id=<?=$game["id"]?>">Meer&nbsp;info</a></a></td>
+        </tr>
+        <?php
+            }
+        ?>
+    </tbody>
+</table>
