402058bd9df4c54ea02ac1d18f6a88153c42748f
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-05-19 10:44:50 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -15,9 +15,9 @@
 ?>
 
 <form action="planningPage.php?id=<?= $game["id"]?>"  method="post">
-    <label for="Confirm"><b>Are you sure you want to delete this planning?</b></label>
-    <button class="btn btn-primary btn-sm" type="submit" name="Delete2" value="true">Yes</button>
-    <button class="btn btn-primary btn-sm" type="submit" name="Delete3" value="false">Cancel</button>
+    <label for="Confirm"><b>Are you sure you want to delete the planning for <?= $game["name"]?>?</b></label>
+    <button class="btn btn-success btn-sm" type="submit" name="Delete2" value="true">Yes</button>
+    <button class="btn btn-danger btn-sm" type="submit" name="Delete3" value="false">Cancel</button>
 </form>
 <?php   
     include("common/footer.php"); 
